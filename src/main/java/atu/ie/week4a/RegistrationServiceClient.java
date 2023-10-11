ea0d24aa7286977e781c810337c7969e71e124b3
G00401971@atu.ie <G00401971@atu.ie>
2023-10-11 14:56:01 +0100

    Created UserService and RegistrationServiceClient interface Class

---
+++ src/main/java/atu/ie/week4a/RegistrationServiceClient.java
@@ -0,0 +1,11 @@
+package atu.ie.week4a;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+public interface RegistrationServiceClient {
+
+
+}
---
+++ src/test/java/atu/ie/week4a/Week4aApplicationTests.java
@@ -0,0 +1,13 @@
+package atu.ie.week4a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week4aApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
